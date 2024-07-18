97228c18f47cbf8e55609b03fd09903085045827
JakeVestal <jmvestal@gmail.com>
2024-07-18 11:07:59 -0400

    0.0.8

--- src/shinybroker/__init__.py
+++ src/shinybroker/__init__.py
@@ -1,4 +1,8 @@
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
+from shinybroker.connection import (
+    create_ibkr_socket_conn, ib_msg_reader_run_loop
+)
+from shinybroker.msgs_to_ibkr import *
 from shinybroker.server import sb_server
 from shinybroker.ui import sb_ui
