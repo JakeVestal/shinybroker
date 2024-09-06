9cfdfd9818d5489c1e67b2537d11dd67fea1693a
JakeVestal <jmvestal@gmail.com>
2024-09-06 08:05:45 -0400

    req_sec_def_opt_params signature vars back to required

---
+++ examples/fetch_sec_def_opt_params.py
@@ -0,0 +1,10 @@
+import shinybroker as sb
+import pandas as pd
+
+sec_def_opt_params = sb.fetch_sec_def_opt_params(
+    underlyingConId=265598,
+    underlyingSymbol="AAPL",
+    underlyingSecType="STK"
+)
+
+print(sec_def_opt_params)
