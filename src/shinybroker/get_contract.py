c540f3b8f4762a0ab8425f717198cfafbe988b26
JakeVestal <jmvestal@gmail.com>
2025-07-18 13:32:31 -0400

    checkpoint

--- src/shinybroker/get_contract.py
+++ src/shinybroker/get_contract.py
@@ -32,28 +32,49 @@ def get_contract_ui(
 
 @module.server
 def get_contract_server(input, output, session, starting_value):
+    contract_matches = reactive.value(
+        {'stocks': pd.DataFrame({}), 'bonds': pd.DataFrame({})}
+    )
 
     @render.ui
     @reactive.event(input.button)
     def matching_contracts():
         cm_df = fetch_matching_symbols(input.search_string())
-        print(cm_df)
+        contract_matches.set(cm_df)
 
         if cm_df['stocks'].shape[0] == 0:
             if cm_df['bonds'].shape[0] == 0:
                 return f"No matches found for: {input.search_string()}"
+            else:
+                return ui.output_data_frame("matching_bonds")
+        else:
+            if cm_df['bonds'].shape[0] == 0:
+                return ui.output_data_frame("matching_stocks")
+            else:
+                return ui.navset_card_tab(
+                    ui.nav_panel(
+                        "Stocks",
+                        ui.output_data_frame("matching_stocks")
+                    ),
+                    ui.nav_panel(
+                        "Bonds",
+                        ui.output_data_frame("matching_bonds")
+                    )
+                )
 
+    @render.data_frame
+    def matching_stocks():
+        return render.DataTable(
+            contract_matches()['stocks'],
+            selection_mode="rows"
+        )
 
-    # @render.data_frame
-    # def matching_contracts():
-    #     return render.DataTable(
-    #         df(),
-    #         width=width,
-    #         height=height,
-    #         filters=input.filters(),
-    #         editable=input.editable(),
-    #         selection_mode=input.selection_mode(),
-    #     )
+    @render.data_frame
+    def matching_bonds():
+        return render.DataTable(
+            contract_matches()['bonds'],
+            selection_mode="rows"
+        )
 
     # @render.code
     # def contract_definition():
