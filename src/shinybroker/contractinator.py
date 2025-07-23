4c5205ec1fc301333ccbe5230cf6c8a08faa926a
JakeVestal <jmvestal@gmail.com>
2025-07-23 09:52:19 -0400

    functional but needs user flow show/hide

--- src/shinybroker/contractinator.py
+++ src/shinybroker/contractinator.py
@@ -7,21 +7,21 @@ from shinybroker import fetch_matching_symbols, Contract, fetch_contract_details
 
 @module.ui
 def contractinator_ui(
-        label: str = "Increment counter",
-        value: str = ""
+        contract_name: str = "",
+        starting_search_string: str = ""
 ):
     text_input = ui.input_text(
         id="search_string",
         label="Enter search string:",
         width="400px",
-        value=value
+        value=starting_search_string
     ).add_style("display:flex;flex-direction:row;align-items:center;")
     text_input.children[0] = text_input.children[0].add_style(
         "width:275px;padding-top:5px;"
     )
 
     return ui.card(
-        ui.card_header(label),
+        ui.card_header(contract_name),
         ui.input_text_area(
             id="contract_definition",
             label="Contract Definition",
@@ -34,7 +34,7 @@ def contractinator_ui(
         ui.output_ui("contract_verification"),
         text_input,
         ui.input_action_button(
-            id="button",
+            id="search_for_matching_contracts_btn",
             label='Search for Matching Contracts'
         ),
         ui.output_ui("matching_contracts")
@@ -43,13 +43,20 @@ def contractinator_ui(
 @module.server
 def contractinator_server(input, output, session):
 
-    # contains formatted results from
+    # namespace of this module as chosen by user
+    module_ns = session.ns('contract_definition').split("-")[0]
+
+    # stores contracts found to match the search string
     contract_matches = reactive.value(
         {'stocks': pd.DataFrame({}), 'bonds': pd.DataFrame({})}
     )
 
+    # 1) When search_for_matching_contracts button is clicked,
+    #   matching_conracts() runs fetch_matching_symbols() on the search
+    #   string and updates the matching_contracts output ui as well as the
+    #   contract_matches() reactive variable.
     @render.ui
-    @reactive.event(input.button)
+    @reactive.event(input.search_for_matching_contracts_btn)
     def matching_contracts():
         cm_df = fetch_matching_symbols(input.search_string())
 
@@ -76,13 +83,15 @@ def contractinator_server(input, output, session):
                     )
                 )
 
+    # 2) The next two render functions make sure that the datatable the user
+    #    is viewing -- stocks or bonds -- is rendered with data from the
+    #    reactive variable "contract_matches".
     @render.data_frame
     def matching_stocks():
         return render.DataTable(
             contract_matches()['stocks'],
             selection_mode="row"
         )
-
     @render.data_frame
     def matching_bonds():
         return render.DataTable(
@@ -90,6 +99,9 @@ def contractinator_server(input, output, session):
             selection_mode="row"
         )
 
+    # 3) If the user selects a row in the stocks datatable, create a contract
+    #    definition out of that row's data and add it into the
+    #    contract_definition text input.
     @reactive.effect
     @reactive.event(matching_stocks.cell_selection)
     def a_stock_row_has_just_been_selected():
@@ -106,7 +118,7 @@ def contractinator_server(input, output, session):
             'description': contract_row['description']
         })
         cdef_string = re.sub(r", ", ",\\n", str(sc))
-        ui.update_text_area('contract_definition', value =cdef_string)
+        ui.update_text_area('contract_definition', value=cdef_string)
 
     @reactive.effect
     @reactive.event(input.validate_contract_btn)
