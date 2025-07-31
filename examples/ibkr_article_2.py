e58e7b13d51aa94fbcdbc463bd42e5b2a37709f9
JakeVestal <jmvestal@gmail.com>
2025-07-31 10:40:38 -0400

    0.6.44 contracinator with specified initials works.

--- examples/ibkr_article_2.py
+++ examples/ibkr_article_2.py
@@ -10,15 +10,9 @@ ui_ = ui.page_fluid(
     })
 )
 
-def server_function_(
-        input: Inputs, output: Outputs, session: Session, ib_socket, sb_rvs
-):
-    sb.contractinator_server(input, output, session)
-
 # Create an instance of a ShinyBroker App object using the default ui and server
 app = sb.sb_app(
     ui_,
-    server_function_,
     host='127.0.0.1',  # localhost TWS is being served on your local machine
     port=7497,         # make this match the port in your API Settings config
     client_id=10742    # picked at random, choose another Client ID if preferred
--- src/shinybroker/contractinator.py
+++ src/shinybroker/contractinator.py
@@ -45,39 +45,3 @@ def contractinator_ui(contract_names: list | dict):
           in contract_names.items()],
         id="contractinator_accordion"
     )
-
-def contractinator_server(input, output, session):
-
-    # 1) When a search_for_matching_contracts button is clicked,
-    #   matching_conracts() runs fetch_matching_symbols() on the search
-    #   string and updates the matching_contracts output ui as well as the
-    #   contract_matches() reactive variable.
-    @reactive.Effect
-    @reactive.event(input.smc_buffer)
-    def contractinator_get_matching_contracts():
-        cm_df = fetch_matching_symbols(
-            input[f"{input.smc_buffer()}_search_string"]()
-        )
-        print(cm_df)
-        # if cm_df['stocks'].shape[0] == 0:
-        #     if cm_df['bonds'].shape[0] == 0:
-        #         return f"No matches found for: {input.search_string()}"
-        #     else:
-        #         contract_matches.set(cm_df)
-        #         return ui.output_data_frame("matching_bonds")
-        # else:
-        #     if cm_df['bonds'].shape[0] == 0:
-        #         contract_matches.set(cm_df)
-        #         return ui.output_data_frame("matching_stocks")
-        #     else:
-        #         contract_matches.set(cm_df)
-        #         return ui.navset_card_tab(
-        #             ui.nav_panel(
-        #                 "Not Bonds",
-        #                 ui.output_data_frame("matching_stocks")
-        #             ),
-        #             ui.nav_panel(
-        #                 "Bonds",
-        #                 ui.output_data_frame("matching_bonds")
-        #             )
-        #         )
