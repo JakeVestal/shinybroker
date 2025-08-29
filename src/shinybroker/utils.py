b651941157a18610fb56712af43566039a088884
JakeVestal <jmvestal@gmail.com>
2025-08-29 14:54:32 -0400

    checkpoint

--- src/shinybroker/utils.py
+++ src/shinybroker/utils.py
@@ -1,8 +1,9 @@
-import struct, datetime
+import struct, datetime, re
 
 import pandas as pd
 
 from shiny import ui
+from shinybroker.obj_defs import Contract
 
 
 def pack_element(msg_emt) -> str:
@@ -99,3 +100,46 @@ def add_contractinator_btns_column(df):
 
     df.reset_index()
     return df
+
+def is_valid_html_id(id_string):
+    # Check if the string is empty
+    if not id_string:
+        return False
+
+    # Check for whitespace
+    if re.search(r'\s', id_string):
+        return False
+
+    # Check if it starts with a letter or underscore, followed by ONLY:
+        # letters, digits, hyphens, or underscores
+    # HTML5 is more permissive, but this approach is safer for CSS and
+        # cross-browser compatibility
+    pattern = r'^[a-zA-Z_][a-zA-Z0-9_-]*$'
+    return bool(re.match(pattern, id_string))
+
+def check_if_string_is_contract_definition(list_or_string_might_be_contracts):
+    # Accepts a string or a list of strings that should contain a contract
+    # definition of the form:
+    #   contract_name = Contract({definition}) or
+    #   contract_name = sb.Contract({definition})
+    # If a sring is passed into list_or_string_might_be_contracts, returns true
+    #   if the string is a valid line of code defining a single contract, false
+    #   otherwise, and prints any exceptions encountered.
+    if isinstance(list_or_string_might_be_contracts, str):
+        might_be_a_contract_def_str = list_or_string_might_be_contracts
+        try:
+            namespace = {}
+            exec('from shinybroker import Contract', namespace)
+            exec(might_be_a_contract_def_str, namespace)
+        except Exception as e:
+            print("There was an error in evaluating the contract definition \""
+                  f"{might_be_a_contract_def_str}\".")
+            print(f"error: {e}")
+            return None
+
+
+    contract_list = []
+
+
+
+    return True
